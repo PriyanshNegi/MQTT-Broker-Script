c9cc6d0369214fe10cf39df7c44f296b7828fec6
Shubhanshu Rao <92927962+shubhanshurao@users.noreply.github.com>
2023-06-18 16:25:29 +0530

    Update test_emqx.py
    made final changes

--- Sending to broker/test_emqx.py
+++ Sending to broker/test_emqx.py
@@ -10,8 +10,8 @@ broker = '--ENTER YOU URL--'
 port = 8883
 topic = 'dataset'
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
-username = '--ENTER USERNAME'
-password = 'ENTER PASSWORD'
+username = '--ENTER USERNAME--'
+password = '--ENTER PASSWORD--'
 
 
 def connect_mqtt():
